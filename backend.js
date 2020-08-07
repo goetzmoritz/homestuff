f4c1c0ec3108079aa9646f9b64843ef56f0aae78
BONG011 <goetzmoritz.bongartz@bertelsmann.de>
2020-08-07 13:40:53 +0200

    Basic stuff added.

--- backend.js
+++ backend.js
@@ -4,3 +4,19 @@
 // Author: goetzmoritz
 // -------------------------------- \\
 
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+dotenv = process.env;
+
+const mongoose = require('mongoose')
+
+mongoose.connect(dotenv.MYDB, { useNewUrlParser: true , useUnifiedTopology: true})
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to mongodb.'))
+app.use(express.json())
+app.listen(dotenv.MYPORT, function(){
+    console.log(`Backend started on port ${dotenv.MYPORT}.`)
+})
